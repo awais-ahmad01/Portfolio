e15898f8808c8fbe99534f7045b13e8a1a3764a2
awais-ahmad01 <aahmad19376@gmail.com>
2024-11-13 09:34:02 -0800

    Build my personal portfolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import { HERO_CONTENT } from '../constants'
+
+import profilePic from '../assets/pic.png'
+
+import { motion } from 'framer-motion';
+
+
+
+const container = (delay)=>({
+    hidden: {x : -100, opacity : 0},
+    visible: {x: 0, opacity: 1,
+        transition: {
+            duration: 0.5,
+            delay: delay
+        }
+    }
+})
+
+
+const Hero = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4 lg:mb-36'>
+
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2">
+                <div className="felx flex-col items-center lg:items-start">
+                    <motion.h1 
+                        variants={container(0)}
+                        initial='hidden'
+                        animate='visible'
+                    className='pb-16 text-6xl font-thin tracking-tight
+                        lg:mt-16 lg:text-8xl'>Awais Ahmad</motion.h1>
+
+                    <motion.div 
+                        variants={container(0.5)}
+                        initial='hidden'
+                        animate='visible'
+                    >
+                        <span 
+                            className='bg-gradient-to-r from-pink-300 via-slate-500
+                                to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
+                            Full Stack Developer
+                        </span>
+                    </motion.div>
+
+                    <motion.p 
+                        variants={container(1)}
+                        initial='hidden'
+                        animate='visible'
+                        className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+                        {HERO_CONTENT}
+                    </motion.p>
+
+                    
+                </div>
+            </div>
+
+            <div className="w-full lg:w-1/2 lg:px-8">
+                <div className="flex justify-center">
+                    <motion.img
+                        initial={{x : 100, opacity: 0}}
+                        animate={{x : 0, opacity : 1}}
+                        transition={{duration: 1, delay: 1.2}}
+                    src={profilePic} alt='pic' className='rounded'/>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `As a highly skilled MERN Stack Developer, I am passionate about creating efficient, scalable, and user-centric web applications. With expertise in React, Node.js, Express, and MongoDB, I aim to leverage my technical skills and creativity to deliver seamless digital solutions. My objective is to continuously grow as a developer while contributing to projects that solve real-world problems and drive business success.`;
+
+export const ABOUT_TEXT = `I am a dedicated Full Stack Developer with a strong focus on the MERN stack (MongoDB, Express, React, and Node.js). I thrive on solving complex challenges and building intuitive web applications that prioritize both performance and user experience. My journey into web development has allowed me to gain a deep understanding of front-end design and back-end architecture, enabling me to work across the full stack with confidence.
+
+Beyond coding, I am always eager to learn new technologies, optimize my workflows, and collaborate on projects that push the boundaries of innovation. When I'm not coding, you’ll find me exploring new tech trends, contributing to open-source projects, or brainstorming the next big idea.
+
+I’m excited to continue honing my skills and to take on challenging projects where I can grow as a developer and contribute to impactful digital products.`;
+
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Tailwind", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "Tailwind", "React"],
+  },
+  {
+    title: "Flickbase",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Bootstrap", "React", "Express", "MongoDB"],
+  },
+
+];
+
+export const CONTACT = {
+  address: "Islamabad, Pakistan",
+  phoneNo: "+92 3149467803 ",
+  email: "aahmad19376@gmail.com",
+};
